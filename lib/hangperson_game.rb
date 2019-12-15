289a7f5359aedbdf23a9b2ea88d55ac8be783d88
Andre Wong <wongandre98@gmail.com>
2019-12-15 15:33:01 +0800

    modified hangperson_game.rb methods

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,9 +7,77 @@ class HangpersonGame
 
   # def initialize()
   # end
+
+  attr_reader :word
+  attr_reader :guesses
+  attr_reader :wrong_guesses
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+
+  def guess(guess_word)
+	if guess_word == '' || guess_word == nil
+		raise ArgumentError
+	end
+
+	if not guess_word.match /[[:alpha:]]/
+		raise ArgumentError
+	end
+
+	@guesses.chars.each do |char|
+		if char == guess_word.downcase
+			return false
+		end
+	end
+
+	@wrong_guesses.chars.each do |char|
+		if char == guess_word.downcase
+			return false
+		end
+	end
+
+	@word.chars.each do |letter|
+		if guess_word == letter
+			@guesses << guess_word.downcase
+			return true
+		end
+	end
+	@wrong_guesses << guess_word
+	return true 
+  end
+
+  def word_with_guesses
+	display = ''
+	appended = false
+	@word.chars.each do |letter|
+		appended = false
+		@guesses.chars.each do |charac|
+			if charac == letter
+				display << letter
+				appended = true
+			end
+		end
+		if not appended
+			display << '-'
+		end
+	end
+	return display
+  end
+
+  def check_win_or_lose
+	if @wrong_guesses.chars.count > 6
+		return :lose
+	else
+		word_with_guesses.chars.each do |char|
+			if char == '-'
+				return :play
+			end
+		end
+	end
+	return :win
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
